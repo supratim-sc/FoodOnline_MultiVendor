8c51ff49853a45919a236159254c93ccafd8ffc7
Supratim Chakraborty <supratim12111996@gmail.com>
2025-03-14 21:51:39 +0530

    made password field non-editable and implemented CustomUserAdmin class

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import User
 
 # Register your models here.
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    # Making password field non-editable
+    fieldsets = []
+    filter_horizontal = []
+    list_filter = []
+
+    # changing the display menu of User model at admin panel
+    list_display = ['email', 'first_name', 'last_name', 'username', 'role', 'is_active']
+
+    # ordering the list of users depending on the date_joined field descending
+    ordering = ['-date_joined']
+    
+
+
+admin.site.register(User, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -83,7 +83,6 @@ class User(AbstractBaseUser):
     last_name = models.CharField(max_length = 50)
     username = models.CharField(max_length = 50, unique = True)
     email = models.EmailField(max_length = 100, unique = True)
-    password = models.CharField(max_length = 100)
     phone_number = models.CharField(max_length = 10, unique = True)
     role = models.PositiveSmallIntegerField(choices = ROLE_CHOICE, default = CUSTOMER)
 
