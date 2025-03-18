6b8f8e9c081f29f8ca4de217f25c9e44a42655a7
Supratim Chakraborty <supratim12111996@gmail.com>
2025-03-18 22:45:50 +0530

    hashing the password and saving the user using create_user() way-2

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,8 @@ def user_registration(request):
         form = UserRegistrationForm(request.POST)
         # checking if all data is valid or not
         if form.is_valid():
+            '''
+            # ----- WAY-1 OF SAVING THE USER USING FORM
             # if data is valid then taking all the data from the form and assign it to the user variable.
             # here commit=False means we are not commititg the changes 
             # as we need to add role to the user hence taking the form data and assign it the the user
@@ -23,6 +25,22 @@ def user_registration(request):
             # getting the password from the form
             password = form.cleaned_data['password']
             user.set_password(password)
+            '''
+
+
+            # ----- WAY-2 OF SAVING THE USER USING create_user() method from the UserManager class
+            # getting the data after cleaning form the form
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            # setting the user from the data from the form
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+
+            # setting the role
+            user.role = User.CUSTOMER
 
             # saving the user with all previous data from the form and the newly assigned role
             user.save()
