d10daf54e0bcc300565e3b0abb2629028a38a260
Supratim Chakraborty <supratim12111996@gmail.com>
2025-03-21 22:53:28 +0530

    SMTP server settings for sending emails

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -147,3 +147,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+# SMTP Configuration
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
