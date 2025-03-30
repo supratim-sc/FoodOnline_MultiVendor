8e784b7a0bf29b71ff6e1acb19543c171b303881
Supratim Chakraborty <supratim12111996@gmail.com>
2025-03-30 20:02:22 +0530

    implement context processor for vendor

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'vendors.vendor_context_processor.get_vendor'
             ],
         },
     },
