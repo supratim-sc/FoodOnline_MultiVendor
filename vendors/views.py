249517f744d66addadb8be116f1c5f363302a3f3
Supratim Chakraborty <supratim12111996@gmail.com>
2025-03-31 20:04:29 +0530

    moved vendor my_restaurant page to vendors folder template

--- vendors/views.py
+++ vendors/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def my_restaurant(request):
-    return render(request, 'accounts/vendor_my_restaurant.html')
+    return render(request, 'vendors/vendor_my_restaurant.html')
